94bca85e8bc1e934c79478c569d8b597c56230e6
Myriantics <myriantics@proton.me>
2025-09-01 11:48:26 -0700

    Initial commit

---
+++ src/client/java/net/myriantics/lootshare/LootShareDataGenerator.java
@@ -0,0 +1,11 @@
+package net.myriantics.lootshare;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LootShareDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
